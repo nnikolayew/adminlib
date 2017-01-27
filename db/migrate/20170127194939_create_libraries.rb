17f1fe6d0efa115a238a88c9c9e81c75de091151
nnikolaev <nnikolayew@gmail.com>
2017-01-27 23:06:42 +0300

    modi

---
+++ db/migrate/20170127194939_create_libraries.rb
@@ -0,0 +1,11 @@
+class CreateLibraries < ActiveRecord::Migration
+  def change
+    create_table :libraries do |t|
+      t.string :number
+      t.string :name
+      t.string :adress
+
+      t.timestamps null: false
+    end
+  end
+end
