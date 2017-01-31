3c42772997ff2847ea38a4242c440a49de1f1213
nnikolaev <nnikolayew@gmail.com>
2017-01-31 14:25:47 +0300

    modification db

--- db/migrate/20170129091813_add_columns_to_tables.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToTables < ActiveRecord::Migration
-  #добавляем недостающие столбцы для таблицы employees
-  def change
-    add_column :employees, :startwork, :date
-    add_column :employees, :position, :string
-    add_column :employees, :education, :string
-  end
-end
